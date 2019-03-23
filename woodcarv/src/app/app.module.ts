d08fe3a577920413820e4e666fb78fc61bbb5c11
Taras <taras.kanafotskyi@nowinfinity.com.au>
2019-03-23 19:45:59 +1000

    Create logo, header and menu components

--- woodcarv/src/app/app.module.ts
+++ woodcarv/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,15 +7,22 @@ import { AppComponent } from './app.component';
 import { registerLocaleData } from '@angular/common';
 import localeUk from '@angular/common/locales/uk';
 import localeUkExtra from '@angular/common/locales/extra/uk';
+import { MainLogoComponent } from './main-logo/main-logo.component';
+import { MenuComponent } from './menu/menu.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 registerLocaleData(localeUk, 'uk', localeUkExtra);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLogoComponent,
+    MenuComponent,
+    SiteHeaderComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ woodcarv/src/app/main-logo/main-logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-logo',
+  templateUrl: './main-logo.component.html',
+  styleUrls: ['./main-logo.component.less']
+})
+export class MainLogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
