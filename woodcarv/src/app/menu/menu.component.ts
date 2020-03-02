1caf74c8ac231ba0475fceacbf88e25a1f8cddf3
Ihor Khvalbota <ihorkhvalbota@gmail.com>
2020-03-03 00:24:02 +0200

    Revert "highlight_the_current_link_in_top_navigation"
    This reverts commit a73fe414a088bf83bcde36dd398b5f4a4eb88651.

--- woodcarv/src/app/menu/menu.component.ts
+++ woodcarv/src/app/menu/menu.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { stripSummaryForJitFileSuffix } from '@angular/compiler/src/aot/util';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.less'],
+  styleUrls: ['./menu.component.less']
 })
 export class MenuComponent implements OnInit {
 
